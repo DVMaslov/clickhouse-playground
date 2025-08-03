37d76cc73f29a023b16286c2c256b074d9f3ae72
DVMaslov <DV.Maslov1990@gmail.com>
2025-08-03 22:12:19 +0300

    drop chown

--- start.sh
+++ start.sh
@@ -8,23 +8,11 @@ set +o allexport
 VOLUMES_DIR=${VOLUMES_DIR:-./volumes}
 CLICKHOUSE_NODES=(node1 node2 node3 node4)
 
-# Requires permission to run Docker commands.
-# Ensure your user is in the 'docker' group or that you are using rootless Docker.
-
-# determine container UIDs/GIDs
-CH_UID=$(docker run --rm "${CH_IMAGE}:${CH_VERSION}" id -u)
-CH_GID=$(docker run --rm "${CH_IMAGE}:${CH_VERSION}" id -g)
-ZK_UID=$(docker run --rm "${ZK_IMAGE}:${ZK_VERSION}" id -u)
-ZK_GID=$(docker run --rm "${ZK_IMAGE}:${ZK_VERSION}" id -g)
-
 # create data directories with appropriate ownership
 for node in "${CLICKHOUSE_NODES[@]}"; do
     mkdir -p "${VOLUMES_DIR}/clickhouse/${node}"
 done
 mkdir -p "${VOLUMES_DIR}/zookeeper/data" "${VOLUMES_DIR}/zookeeper/datalog"
-
-chown -R "${CH_UID}:${CH_GID}" "${VOLUMES_DIR}/clickhouse"
-chown -R "${ZK_UID}:${ZK_GID}" "${VOLUMES_DIR}/zookeeper"
 
 # start cluster
 docker compose up -d
